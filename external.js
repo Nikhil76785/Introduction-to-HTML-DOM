1422474d001bb35aaecd99205e7d8fc81a50c098
Nikhil76785 <valivetinikhil2@gmail.com>
2025-03-15 20:17:13 +0530

    JJSSSS

---
+++ external.js
@@ -0,0 +1,8 @@
+function abc() {
+    var a = 75;
+    document.getElementById("demo").innerHTML = a*a;
+}
+function ab() {
+    var b = 75;
+    document.getElementById("demo2").innerHTML = b*b*b;
+}
